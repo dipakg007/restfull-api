f490e35d29560331742a1029c596e38480f23d63
dipakg007 <dipagg286@gmail.com>
2022-08-22 13:38:49 +0530

    Added sort by and pagination support

--- src/main/java/com/springboot/blog/service/PostService.java
+++ src/main/java/com/springboot/blog/service/PostService.java
@@ -1,17 +1,16 @@
 package com.springboot.blog.service;
 
-import java.util.List;
-
 import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.dto.PostResponse;
 
 public interface PostService {
 	PostDto createPost(PostDto postDto);
 
-	List<PostDto> getAllPosts();
-	
+	PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir);
+
 	PostDto getPostById(Long id);
-	
-	PostDto updatePost(PostDto postDto,Long id);
-	
+
+	PostDto updatePost(PostDto postDto, Long id);
+
 	void deletePost(Long id);
 }
--- src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/springboot/blog/service/impl/PostServiceImpl.java
@@ -5,9 +5,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.springboot.blog.dto.PostDto;
+import com.springboot.blog.dto.PostResponse;
 import com.springboot.blog.entity.Post;
 import com.springboot.blog.exception.ResourceNotFoundException;
 import com.springboot.blog.repository.PostRepository;
@@ -27,33 +31,55 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public List<PostDto> getAllPosts() {
-		List<Post> posts = postRepository.findAll();
-		return posts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+	public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy, String sortDir) {
+		// Create pageable instance
+
+		Sort sort = sortDir.equalsIgnoreCase(Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending()
+				: Sort.by(sortBy).descending();
+		PageRequest pageable = PageRequest.of(pageNo, pageSize, sort);
+
+		Page<Post> posts = postRepository.findAll(pageable);
+
+		// get content for page object
+		List<Post> listOfPosts = posts.getContent();
+
+		List<PostDto> content = listOfPosts.stream().map(post -> mapToDto(post)).collect(Collectors.toList());
+		PostResponse postResponse = new PostResponse();
+		postResponse.setContent(content);
+		postResponse.setPageNo(posts.getNumber());
+		postResponse.setPageSize(posts.getSize());
+		postResponse.setTotalElements(posts.getTotalElements());
+		postResponse.setTotalPages(posts.getTotalPages());
+		postResponse.setLast(posts.isLast());
+
+		return postResponse;
 	}
 
 	@Override
 	public PostDto getPostById(Long id) {
-		Optional<Post> _post = Optional.ofNullable(postRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id)));
+		Optional<Post> _post = Optional.ofNullable(
+				postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id)));
 		return mapToDto(_post.get());
 	}
-	
+
 	@Override
 	public PostDto updatePost(PostDto postDto, Long id) {
-		Optional<Post> _post = Optional.ofNullable(postRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id)));
+		Optional<Post> _post = Optional.ofNullable(
+				postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id)));
 		Post post = _post.get();
-		
+
 		post.setTitle(postDto.getTitle());
 		post.setContent(postDto.getContent());
 		post.setDescription(postDto.getDescription());
-		
+
 		Post updatedPost = postRepository.save(post);
 		return mapToDto(updatedPost);
 	}
-	
+
 	@Override
 	public void deletePost(Long id) {
-		Optional<Post> _post = Optional.ofNullable(postRepository.findById(id).orElseThrow(()->new ResourceNotFoundException("Post","id",id)));
+		Optional<Post> _post = Optional.ofNullable(
+				postRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Post", "id", id)));
 		postRepository.deleteById(id);
 	}
 
@@ -76,9 +102,5 @@ public class PostServiceImpl implements PostService {
 		post.setContent(postDto.getContent());
 		return post;
 	}
-
-	
-
-	
 
 }
